8f5f746dae1b18c7c26b1d34b9b1181f596cd522
Athukorala A.T.N <nejantharana2004.pvt@gmail.com>
2026-03-17 10:32:21 +0530

    edited some json files

--- mahasona-squad/vite.config.js
+++ mahasona-squad/vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/mahasona-squad/",
+  base: "/ms-front-end/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
